d45ba7aab7998ab8891ae0624a5efeb9c174220e
dishitamohan <92173465+dishitamohan@users.noreply.github.com>
2025-03-26 10:08:11 +0530

    Update rule_extraction.py

--- src/rule_extraction.py
+++ src/rule_extraction.py
@@ -1,10 +1,11 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCiGZUTd4MheB995OM9Yjk7cgeq6AmETNs")
+genai.configure(api_key="AIzaSyDO7BHvUTLChkYFUlCSJK3eDUDN2zQj85E")
 
 def extract_rules_gemini(instruction_text):
     model = genai.GenerativeModel('models/gemini-1.5-pro-002')
     response = model.generate_content(instruction_text)
-    with open("Team_Repo2\data\extracted_rules.py", "w") as f:
+    with open("D:\DataProfiling_TechnologyHackathon\data_profiling-main\data_profiling-main\data\extracted_rules.py", "w") as f:
         f.write(response.text)
+    print(response.text)
     return response.text
